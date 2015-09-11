c1f4f6fc2d7606a4251c860217bc346f9df56a20
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2015-09-11 17:14:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 for dotfile in `ls -A1 files`
 do
   echo $dotfile
-  ln -bs `pwd`/files/$dotfile ~
+  ln -is `pwd`/files/$dotfile ~
 done
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
